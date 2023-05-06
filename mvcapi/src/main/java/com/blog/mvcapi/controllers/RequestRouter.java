425944511901584e90cb41bf48d15f1aa3a35327
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-05-07 00:58:08 +0530

    added server port configuration, logFile & maximum thread pool size of tomcat server
    Added Request Router(Controller) and a service layer class

---
+++ mvcapi/src/main/java/com/blog/mvcapi/controllers/RequestRouter.java
@@ -0,0 +1,25 @@
+package com.blog.mvcapi.controllers;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.library.Request;
+import com.blog.mvcapi.services.Requestor;
+
+@RestController
+@RequestMapping("/mvc")
+public class RequestRouter {
+
+    @Autowired
+    private Requestor requestor;
+
+    @PostMapping("getLog")
+    public String getResponse(@RequestBody Request request) throws InterruptedException, IOException {
+        return requestor.logRequest(request);
+    }
+}
---
+++ mvcapi/src/main/java/com/blog/mvcapi/services/Requestor.java
@@ -0,0 +1,25 @@
+package com.blog.mvcapi.services;
+
+import java.io.IOException;
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Service;
+
+import com.blog.library.Request;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class Requestor {
+
+    public String logRequest(Request request) throws InterruptedException, IOException {
+        LocalTime timeStamp = LocalTime.now();
+        Thread.sleep(5000);
+        String response = String.format("Request:- %s, created at:- %s accepted at:- %s returns at %s",
+                request.getId(), request.getCreationTime(), timeStamp.toString(), LocalTime.now());
+        log.info(response);
+        return response;
+    }
+
+}
