77b6268ee9b72ab2ffec588f4337e7cf88431272
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-05-07 17:47:25 +0530

    Added a custom LoopResource with three threads

--- reactiveapi/src/main/java/com/blog/reactiveapi/ReactiveapiApplication.java
+++ reactiveapi/src/main/java/com/blog/reactiveapi/ReactiveapiApplication.java
@@ -2,10 +2,12 @@ package com.blog.reactiveapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
-import io.netty.channel.nio.NioEventLoopGroup;
+import reactor.netty.resources.LoopResources;
 
 @SpringBootApplication
 @EnableWebFlux
@@ -15,8 +17,16 @@ public class ReactiveapiApplication {
 		SpringApplication.run(ReactiveapiApplication.class, args);
 	}
 
+	// @Bean
+	// public NioEventLoopGroup eventLoopGroup(){
+	// return new NioEventLoopGroup(3);
+	// }
+
 	@Bean
-	public NioEventLoopGroup eventLoopGroup(){
-		return new NioEventLoopGroup(3);
+	public ReactiveWebServerFactory reactiveWebServerFactory() {
+		NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
+		factory.addServerCustomizers(builder -> builder.runOn(LoopResources.create("my-http", 3, true)));
+
+		return factory;
 	}
 }
