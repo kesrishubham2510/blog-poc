4afcf9c0da72efa0d1bfce2289d15e807ea4f648
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-05-07 11:56:06 +0530

    Added configuration for thread pool size on the netty server.

--- reactiveapi/src/main/java/com/blog/reactiveapi/ReactiveapiApplication.java
+++ reactiveapi/src/main/java/com/blog/reactiveapi/ReactiveapiApplication.java
@@ -2,11 +2,21 @@ package com.blog.reactiveapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+import io.netty.channel.nio.NioEventLoopGroup;
 
 @SpringBootApplication
+@EnableWebFlux
 public class ReactiveapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveapiApplication.class, args);
+	}
+
+	@Bean
+	public NioEventLoopGroup eventLoopGroup(){
+		return new NioEventLoopGroup(3);
 	}
 }
