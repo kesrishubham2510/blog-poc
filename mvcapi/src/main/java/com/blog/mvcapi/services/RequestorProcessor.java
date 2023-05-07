5252f5a6d6f23649a6de61dfa65f5dceac30dbb0
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-05-07 17:45:01 +0530

    Added a blocking call to mimic another API call

--- mvcapi/src/main/java/com/blog/mvcapi/services/RequestorProcessor.java
+++ mvcapi/src/main/java/com/blog/mvcapi/services/RequestorProcessor.java
@@ -1,7 +1,8 @@
 package com.blog.mvcapi.services;
 
-import java.io.IOException;
 import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.stereotype.Service;
 
@@ -13,11 +14,24 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class RequestorProcessor {
 
-    public String logRequest(Request request) throws InterruptedException, IOException {
+    public String logRequest(Request request) throws InterruptedException, ExecutionException{
         LocalTime timeStamp = LocalTime.now();
-        Thread.sleep(5000);
+        
+        // Mimicing an api call which takes 5 seconds to complete
+        CompletableFuture<String> mimicCall = CompletableFuture.supplyAsync(()->{
+             try {
+                Thread.sleep(5000);
+                return String.valueOf(request.getId());
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+              return "Exceptionn occured";
+        });
+        mimicCall.get();
+
         String response = String.format("Request:- %s, created at:- %s accepted at:- %s returns at %s",
-                request.getId(), request.getCreationTime(), timeStamp.toString(), LocalTime.now());
+                mimicCall.get(), request.getCreationTime(), timeStamp.toString(), LocalTime.now());
         log.info(response);
         return response;
     }
